b17c51c9eff3930b825c7724c971bf0738955ce5
Erik Valkering <erikvalkering@gmail.com>
2023-07-23 17:42:14 +0200

    Now printing contents of a pdf file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let bytes = std::fs::read("thesis.pdf").unwrap();
+    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
+    println!("{}", out);
 }
