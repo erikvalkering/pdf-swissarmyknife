a90652c1e208b3a56df71a9aa460799ece560cbe
Erik Valkering <erikvalkering@gmail.com>
2023-07-28 17:46:35 +0200

    Ignore invalid words

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ fn main() {
         let text = doc.extract_text(&[page_number]).unwrap_or_else(|_| panic!("Unable to extract text from page {} from PDF", page_number));
 
         for word in text.split_whitespace() {
+            let word = word.trim_matches(|c| "()[]".contains(c));
+            if word.is_empty() { continue; }
+            if !word.chars().all(char::is_alphabetic) { continue; }
             let key = word.to_lowercase();
             if !words.contains(&key) { continue; }
 
