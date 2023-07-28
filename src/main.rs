a5a26ea76242a57852fca4232ab61910f952e0fb
Erik Valkering <erikvalkering@gmail.com>
2023-07-28 08:42:48 +0200

    Now only indexing a (hardcoded) set of words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::collections::HashMap;
 
 use itertools::Itertools;
@@ -7,21 +8,30 @@ use lopdf::{Document, Error};
 fn main() -> Result<(), Error> {
     let doc = Document::load("thesis.pdf")?;
 
-    let mut index: HashMap<String, Vec<u32>> = HashMap::new();
+    let words: HashSet<_> = vec!["nurbs".to_owned(), "ray".to_owned(), "erik".to_owned()].into_iter().collect();
+
+    let mut index: HashMap<String, Vec<(String, u32)>> = HashMap::new();
     for (page_number, _) in doc.get_pages() {
         let text = doc.extract_text(&[page_number])?;
 
         for word in text.split_whitespace() {
-            let word = word.to_lowercase();
-
-            let entry = &mut *index.entry(word).or_insert(vec![]);
-            entry.push(page_number);
+            let key = word.to_lowercase();
+            if words.contains(&key) {
+                let entry = &mut *index.entry(key).or_insert(vec![]);
+                entry.push((word.to_owned(), page_number));
+            }
         }
     }
 
-    for (word, pages) in index {
-        let page_numbers = pages.iter().join(", ");
-        println!("{}: {}", word, page_numbers);
+    for pages in index.values() {
+        let unique_words: HashSet<_> = pages.iter().map(|(word, _)| word).collect();
+        let page_numbers: HashSet<_> = pages.iter().map(|(_, page)| page).collect();
+
+        println!(
+            "{}: {}",
+            unique_words.iter().join(", "),
+            page_numbers.iter().join(", "),
+        );
     }
 
     Ok(())
