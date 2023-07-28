b1c296ce60dd8b3710d4f025320ccaf3e16add08
Erik Valkering <erikvalkering@gmail.com>
2023-07-28 07:23:57 +0200

    Switched to lopdf crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use lopdf::{Document};
+
+fn extract_text_from_page(pdf_path: &str, page_number: u32) -> Result<String, Box<dyn std::error::Error>> {
+    let doc = Document::load(pdf_path)?;
+
+    let text = doc.extract_text(&[page_number])?;
+    Ok(text)
+}
+
 fn main() {
-    let bytes = std::fs::read("thesis.pdf").unwrap();
-    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
-    println!("{}", out);
+    let out = extract_text_from_page("thesis.pdf", 1).unwrap();
+    for word in out.split_whitespace() {
+        println!("{}", word);
+    }
 }
