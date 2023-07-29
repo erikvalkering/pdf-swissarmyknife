6d64552271824f49656ac957b775f380e42c956b
Erik Valkering <erikvalkering@gmail.com>
2023-07-29 10:26:58 +0200

    Added command line parser

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,41 @@ use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
+use clap::Parser;
+
 use itertools::Itertools;
 
 use lopdf::Document;
 
-fn main() {
-    let doc = Document::load("thesis.pdf").expect("Unable to open PDF");
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to PDF to generate an index for
+    #[arg(short, long)]
+    pdf: String,
 
-    let words_file = File::open("words.txt").expect("Cannot open words.txt containing the words to look for in the PDF");
+    /// Path to file containing the words to be considered
+    #[arg(short, long)]
+    words: Option<String>,
+}
+
+fn load_words(path: &str) -> HashSet<String> {
+    let words_file = File::open(path).expect("Cannot open words.txt containing the words to look for in the PDF");
     let words: HashSet<_> = BufReader::new(words_file)
         .lines()
         .map(|x| x.expect("Unable to parse a line from the words.txt"))
         .map(|word| word.to_lowercase())
         .collect();
+
+    words
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let doc = Document::load(args.pdf).expect("Unable to open PDF");
+
+    let words = args.words.map(|x| load_words(&x));
 
     let mut index: BTreeMap<String, Vec<(String, u32)>> = BTreeMap::new();
     for (page_number, _) in doc.get_pages() {
@@ -27,9 +49,14 @@ fn main() {
             if !word.chars().all(char::is_alphabetic) { continue; }
 
             let key = word.to_lowercase();
-            if !words.contains(&key) { continue; }
+            if match &words {
+                Some(words) => !words.contains(&key),
+                None => false,
+            } {
+                continue;
+            }
 
-            let entry = &mut *index.entry(key).or_insert(vec![]);
+            let entry: &mut Vec<(String, u32)> = &mut *index.entry(key).or_insert(vec![]);
             entry.push((word.to_owned(), page_number));
         }
     }
