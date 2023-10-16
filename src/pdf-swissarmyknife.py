cd873db637422dda73f9c88087851a64e04d66de
Erik Valkering <erikvalkering@gmail.com>
2023-10-16 20:20:30 +0200

    added progress bar

--- src/pdf-swissarmyknife.py
+++ src/pdf-swissarmyknife.py
@@ -1,9 +1,10 @@
 from PyPDF2 import PdfReader
+from alive_progress import alive_it
 import sys
 import json
 
 filename = sys.argv[1]
 reader = PdfReader(filename)
-text = [page.extract_text() for page in reader.pages]
+text = [page.extract_text() for page in alive_it(reader.pages)]
 json.dump(text, open(filename + '.json', 'w'))
 
